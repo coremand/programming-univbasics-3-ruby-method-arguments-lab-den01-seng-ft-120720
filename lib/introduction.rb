158ba64d1b3aeee158844a0c81e556068f4018ba
Obinna Nwabia <valinevite@gmail.com>
2020-11-07 07:03:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,9 +11,9 @@ introduction_with_language("Marcus", "Ruby")
 
 
 def introduction_with_language_optional (name, language = "Ruby"
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
+introduction_with_language_optional("Derrick")
 
 
 
